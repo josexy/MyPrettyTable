f3628b562b19078c609bb7e43368b532f80069ab
josexy <josephxrays@163.com>
2020-02-06 13:30:18 +0800

    update

--- src/PrettyTable.h
+++ src/PrettyTable.h
@@ -26,7 +26,24 @@ using std::right;
 // Default space takes two bytes
 #define PADDING_LEFT_RIGHT 2
 
-class Exception :public  exception {
+#ifdef WIN32
+#define PTF_WINDOWS WIN32
+#endif
+#ifdef __linux__
+#define PTF_LINUX __linux__
+#endif
+#if __unix__
+#define PTF_UNIX __unix__
+#endif
+#if __APPLE__
+#define PTF_MAC __APPLE__
+#endif
+
+#ifdef PTF_WINDOWS
+#pragma warning(disable:4996)
+#endif
+
+class Exception :public exception {
 private:
     string m_str_error;
 public:
@@ -35,13 +52,11 @@ public:
         return m_str_error.c_str();
     }
 };
-
 /**
  *  PrettyTable can format and print a table from data
  */
 class PrettyTable {
 public:
-
     // align type
     enum class Align {
         Left,
@@ -60,22 +75,6 @@ public:
         Align _align;
         StringArray _header;
     };
-private:
-    stringstream m_innerOss;
-    int m_Rows;
-    int m_Columns;
-    // header
-    Header m_Header;
-    // all rows
-    MultiStringArray m_multiRows;
-    // border style
-    BorderStyle m_BorderStyle;
-    // all columns
-    MultiStringArray m_multiColumns;
-    vector<int>m_ColumnsContent_max_len;
-    bool is_DrawTable;
-    ofstream m_FileOut;
-    bool is_cancelFrame;
 public:
     explicit PrettyTable();
     ~PrettyTable();
@@ -91,13 +90,16 @@ public:
      */
     void SetBorderStyle(Sign _Corner = Sign::PT_PLUS, Sign _Border_h = Sign::PT_V, Sign _Border_v = Sign::PT_H);
     /**
-     * Return a string table
+     *  Return a string table
+     * @param _start
+     * @param _end
      * @return
      */
     String to_String(int _start = 1, int _end = -1);
     /**
      * Add a header, it is necessary
      * @param _header
+     * @param _align
      */
     void AddHeader(const StringArray& _header, Align _align = Align::Left);
     /**
@@ -112,6 +114,7 @@ public:
     void AddRows(const MultiStringArray& _rows);
     /**
      * Add a column
+     * @param _header_title
      * @param _column
      */
     void AddColumn(const String& _header_title, const StringArray& _column);
@@ -119,60 +122,24 @@ public:
      * Start draw a table
      */
     void DrawTable();
-
     /**
      * Set table content aligning
      */
     void SetAlign(Align align = Align::Left);
-
     /**
      *  Save to file on disk
      * @param _file_path
      */
     void OutputFile(const string& _file_path = "output_table.txt");
-
-    /**
-     * Clean all data
-     */
-    void Cleanup();
-
     friend ostream& operator<<(ostream& out, PrettyTable& pt) {out << pt.to_String();return out;}
-
-private:
-    /**
-     *  build header such as
-     */
+protected:
     void _draw_header();
-    /**
-     *  draw alll rows
-     */
     void _draw_rows(int, int);
-
-    /**
-     *  draw a complete table
-     */
     void _draw_table(int, int);
-    /**
-     *  get max  size() in columns stringArray
-     * @return
-     */
-    vector<int> _getMaximum_strLength();
-    /**
-     *  convert one row to column
-     * @param _row
-     */
-    void _RowConvertColumn(const StringArray& _row);
-    /**
-     *  convert all rows to a columns array
-     */
-    void _RowsConvertColumns();
+    const vector<int>& _getMaximum_strLength();
 
-    /**
-     * ALIGNING
-     * Align::Left  Align::Right
-     * @param n
-     * @param align
-     */
+    void _RowConvertColumn(const StringArray& _row);
+    void _RowsConvertColumns();
     void padding(int n, Align align = Align::Left) {
         switch (align) {
             case Align::Left: {
@@ -181,9 +148,11 @@ private:
             case Align::Right: {
                 m_innerOss << right << setw(n);
             }break;
+            default: {
+                m_innerOss << left << setw(n);
+            }break;
         }
     }
-
     void _put_chars(const Sign c, int n) {
         for (int i = 0; i < n; ++i) {
             _put_char(c);
@@ -192,11 +161,11 @@ private:
     void _put_char(const Sign c) {
         m_innerOss << (static_cast<char>(c));
     }
-    void _put_endline() {
-        m_innerOss << endl;
-    }
+    void _put_endline(){m_innerOss << endl;}
+    void _cleanup();
+
     /**
-     *  deal with Chinese characters
+     *  Deal with Chinese characters
      * @param str
      * @return
      */
@@ -204,7 +173,8 @@ private:
     /**
      *  In UTF-8,
      *  If string include Chinese characters, it will obtain obvious characters
-     *  Such as string s="你好cpp", ouput 9-2=7
+     *  Such as string s="你好cpp" :ouput 9-2=7
+     *  But in Windows, this is will output: 7
      * @param raw_str
      * @return
      */
@@ -215,6 +185,18 @@ private:
      * @return
      */
     int _get_Chinese_len(const string& str);
+private:
+    stringstream m_innerOss;
+    int m_Rows;
+    int m_Columns;
+    Header m_Header;
+    MultiStringArray m_multiRows;
+    BorderStyle m_BorderStyle;
+    MultiStringArray m_multiColumns;
+    vector<int>m_ColumnsContent_max_len;
+    ofstream m_FileOut;
+    bool is_DrawTable;
+    bool is_cancelFrame;
 };
 
-#endif //PRETTYTABLE_PRETTYTABLE_H
+#endif
